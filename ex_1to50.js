300012e61ea7be40c66d6114a60f330d55f6e235
JH <2021620059@sdu.ac.kr>
2023-02-14 03:31:35 +0900

    230214_01

--- ex_1to50.js
+++ ex_1to50.js
@@ -1,36 +1,89 @@
 const sBtn=document.querySelector('#start');
 const rBtn=document.querySelector('#restart');
-const arrA=new Array(25);
-const arrB=new Array(25);
+const over=document.querySelector('#gameOver');
+const game=document.querySelector('.game');
+const arrA=[];
+const arrB=[];
 
-function makeRanArr(){
-    for(let i=1; i<51; i++){
-        let idx=Math.random(24);
-        if(i==1){
-            arrA[idx]=i;
-        }else if(i>1 && i<25){
-            if(arrA[idx]!==null){
-                i--;
-                continue;
-            }else{
-                arrA[idx]=i;
-            }
-        }else if(i==25){
-                arrB[idx]=i;
+sBtn.addEventListener('click',()=>{
+    shuffleArr();
+    showArr();
+    sBtn.style="display: none";
+    playGame();
+})
+
+rBtn.addEventListener('click',()=>{
+    rBtn.style="display: none";
+    over.style="display: none";
+    shuffleArr();
+    showArr();
+    playGame();
+})
+
+function shuffleArr(){
+    let idx=1;
+    for(let i=0; i<25; i++){
+        arrA[i]=idx;
+        arrB[i]=idx+25;
+        idx++;
+    }
+    
+    for(let i=0; i<100; i++){
+        let ran=Math.floor(Math.random()*25);
+        let temp;
+        if(i%2==0){
+            temp=arrA[0];
+            arrA[0]=arrA[ran];
+            arrA[ran]=temp;
         }else{
-            if(arrB[idx]!==null){
-                i--;
-                continue;
-            }else{
-                arrA[idx]=i;
-            }
+            temp=arrB[0];
+            arrB[0]=arrB[ran];
+            arrB[ran]=temp;
         }
     }
 }
 
-sBtn.addEventListener('click',()=>{
-    makeRanArr();
-    sBtn.style="display: none";
-    rBtn.style="display: block";
-})
+function showArr(){
+    let table=document.createElement('table');
+    let text="";
+    for(let i=0; i<25; i++){
+        if(i%5==0){
+        text+=`<tr><td>${arrA[i]}</td>`
+    }else if(i%5==4){
+        text+=`<td>${arrA[i]}</td></tr>`
+    }else{
+        text+=`<td>${arrA[i]}</td>`
+    }
+}
+    table.innerHTML=text;
+    game.appendChild(table);
+}
 
+function playGame(){
+    const tdList=document.querySelectorAll('td');
+    let idx=0;
+    let cnt=26;
+    tdList.forEach(td=>{
+        td.addEventListener('click',()=>{
+            if(idx<25){
+                if(td.innerHTML==idx+1){
+                    td.classList.add('arrB');
+                    td.innerHTML=arrB[idx];
+                    idx++;
+                }
+            }else{
+                if(td.innerHTML==cnt){
+                    td.className='done';
+                    td.innerHTML="";
+                    cnt++;
+                    if(cnt==51){
+                        over.style="display: block";
+                        rBtn.style="display: block";
+                        game.removeChild(game.firstChild);
+                        return;
+                    }
+                }
+            }
+        })
+    })
+}
